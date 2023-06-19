f50530dfdad25ad430bd8ddbfc6a5390253d4ea9
Eissa Monet <eissamonet@Eissas-Air.attlocal.net>
2023-06-18 21:24:12 -0500

    added if else statement for number of characters

--- Assets/script.js
+++ Assets/script.js
@@ -5,8 +5,20 @@ function generatePassword(){
   var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numeriCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "<", ":", ">", "/", "?", "~", ";"]
-
 }
+
+// Ask user for their input
+numberOfCharacters = prompt("Choose between 8-128 characters for your password.");
+if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+  return "Invalid number of characters.";}
+  else if (isNaN(numberOfCharacters)) {
+    numberOfCharacters = prompt("Enter a valid number.");
+  }
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input

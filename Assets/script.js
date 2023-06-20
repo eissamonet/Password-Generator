a26c0c62b9942ff3f19c601d789e0645092144b9
Eissa Monet <eissamonet@Eissas-Air.attlocal.net>
2023-06-19 21:42:28 -0500

    added alert to valid number of characters

--- Assets/script.js
+++ Assets/script.js
@@ -3,10 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log ("hey");
+  // Array of password options/characters
+  var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W","X", "Y", "Z"];
+  var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "<", ":", ">", "/", "?", "~", ";"];
 
 // 1. Prompt the user for the password criteria
 //   a. password length 8-128 characters
 //   b. lowercase, uppercase, numbers, special characters
+numberOfCharacters = prompt("Choose between 8-128 characters for your password.");
+if (numericCharacters < 8 || numericCharacters > 128) {
+  return "Choose valid number of characters.";
+} else if (is NaN(numberOfCharacters)) {
+  numberOfCharacters = prompt("Enter a valid number.");
+}
+else {
+  alert("Your password will be" + numericCharacters + "characters long.");
+}
+
 // 2. Validate input
 // 3. Generate password based on criteria
 
@@ -15,11 +30,8 @@ function generatePassword() {
   return "Generated password will go here!";
 }
 
-// Array of password options/characters
-  var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W","X", "Y", "Z"];
-  var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "<", ":", ">", "/", "?", "~", ";"];
+
+ 
 
 
 // Write password to the #password input
